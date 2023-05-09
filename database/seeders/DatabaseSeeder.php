eb895a907b3e1655e4bf05e440decaff3da9e9ee
Laurin49 <welter@diwe.info>
2023-05-09 19:25:24 +0200

    dev06_posts o.k.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(AdminSeeder::class);
+        \App\Models\Post::factory(10)->create();
+        // $this->call(AdminSeeder::class);
     }
 }
